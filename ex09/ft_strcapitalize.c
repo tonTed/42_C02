bcf18742930361c325cd6d9827c0d706bd1876f2
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-14 16:39:38 -0500

    ex09 ft_strcapitalize.c works

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 21:22:18 by tonted            #+#    #+#             */
-/*   Updated: 2021/02/13 21:25:33 by tonted           ###   ########.fr       */
+/*   Updated: 2021/02/14 16:38:58 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,48 @@
 #include <string.h>
 #include <stdio.h>
 
+int	is_alpha(char *c)
+{
+	if ((*c >= 'a' && *c <= 'z') || (*c >= 'A' && *c <= 'Z'))
+		return (1);
+	return (0);
+}
+
 char	*ft_strcapitalize(char *str)
 {
+	int i;
+	int start_word;
 
+	i = -1;
+	start_word = 1;
+	while(str[++i] != '\0')
+	{
+		if (is_alpha(&str[i]))
+		{
+			if(start_word == 1 && str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] -= 32;
+				start_word--;
+			}
+			else if (start_word != 1 && str[i] >= 'A' && str[i] <= 'Z')
+				str[i] += 32;
+			else
+				start_word--;
+		}
+		else
+		{
+			start_word = 1;
+		}
+	}
+	return (str);
 }
 
 int		main(void)
 {
+	char string[] = "salut, comment tu vA ? 42mots QUArante-deux; cinquAnte+et+un";
+
+	ft_strcapitalize(string);
+	printf("%s\n", string);
+
 	return 0;
 }
