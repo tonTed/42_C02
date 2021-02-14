a9567152b846aead46d9f20c8f6f8ef27262686f
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-14 14:35:42 -0500

    ex01 ft_strncpy norminette ok

--- ex01/test.c
+++ ex01/test.c
@@ -6,18 +6,32 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:56:56 by tonted            #+#    #+#             */
-/*   Updated: 2021/02/14 14:35:08 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/14 14:33:00 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
-** Reproduire à l’identique le fonctionnement de la fonction strncpy
-** (man strncpy).
+** Reproduire à l’identique le fonctionnement de la fonction strncpy (man strncpy).
 */
+
+#include <string.h>
+#include <stdio.h>
+
+int ft_strlen(char *str)
+{
+    int i;
+
+    i = 0;
+    while(str[i] != '\0')
+        i++;
+    return i;
+}
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
 	int i;
+	
+	printf("size = %d\n", n);
 
 	i = -1;
 	while (++i < n && src[i] != '\0')
@@ -25,4 +39,20 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 	while (i < n)
 		dest[i++] = '\0';
 	return (dest);
+}
+
+int 	main(void)
+{
+	char src[] = "Eco";
+	char dest[] = "Schoollll";
+
+	printf("src = %s // dest = %s\n", src, dest);
+	strncpy(&dest[0], &src[0], 5);
+	printf("src = %s // dest = %s \n", src, dest);
+
+	char dest2[] = "Schoollll";
+	ft_strncpy(&dest2[0], &src[0], 5);
+	printf("src = %s // dest = %s \n", src, dest2);
+	
+	return 0;
 }
