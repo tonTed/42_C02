6579fd0f388730dbcce34eedba3e6a30d73ef341
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-14 11:22:25 -0500

    ex07 ft_strupcase works

--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strupcase.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
+/*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 21:13:42 by tonted            #+#    #+#             */
-/*   Updated: 2021/02/13 21:14:37 by tonted           ###   ########.fr       */
+/*   Updated: 2021/02/14 11:21:30 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,37 @@
 #include <string.h>
 #include <stdio.h>
 
+
 char	*ft_strupcase(char *str)
 {
+	int i;
 
+	i = -1;
+	while (str[++i] != '\0')
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
+	return (str);
 }
 
 int		main(void)
 {
+	char string1[] = "nklanoisdn";
+	char string2[] = "dakAFgla92";
+	char string3[] = "jfiend-.fsd";
+	char string4[] = "zzASgaaza";
+	char string5[] = "qk ki2 '";
+
+	ft_strupcase(string1);
+	ft_strupcase(string2);
+	ft_strupcase(string3);
+	ft_strupcase(string4);
+	ft_strupcase(string5);
+
+	printf("%s\n", string1);
+	printf("%s\n", string2);
+	printf("%s\n", string3);
+	printf("%s\n", string4);
+	printf("%s\n", string5);
+
 	return 0;
 }
