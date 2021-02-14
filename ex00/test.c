8f52a95ae0611237f363157c83205c815f90635c
tonted <yddetco@gmail.com>
2021-02-13 21:56:23 -0500

    ex00 OK

--- ex00/test.c
+++ ex00/test.c
@@ -6,13 +6,16 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:54:45 by tonted            #+#    #+#             */
-/*   Updated: 2021/02/13 21:55:41 by tonted           ###   ########.fr       */
+/*   Updated: 2021/02/13 21:52:32 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*
 ** Reproduire à l’identique le fonctionnement de la fonction strcpy (man strcpy).
 */
+
+#include <string.h>
+#include <stdio.h>
 
 char	*ft_strcpy(char *dest, char *src)
 {
@@ -23,4 +26,16 @@ char	*ft_strcpy(char *dest, char *src)
 		dest[i] = src[i];
 	dest[i] = '\0';
 	return (dest);
+}
+
+int		main(void)
+{
+	char src[] = "Teddy";
+	char dest[6];
+
+	printf("src = %s // dest = %s\n", src, dest);
+	ft_strcpy(&dest[0], &src[0]);
+	printf("src = %s // dest = %s\n", src, dest);
+	
+	return 0;
 }
