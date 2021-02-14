cfaf55a43f45123894424bf86ae4a5770d9c222b
tonted <yddetco@gmail.com>
2021-02-13 23:10:53 -0500

    ex02 with test

--- ex02/test.c
+++ ex02/test.c
@@ -6,7 +6,7 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:58:49 by tonted            #+#    #+#             */
-/*   Updated: 2021/02/13 23:08:54 by tonted           ###   ########.fr       */
+/*   Updated: 2021/02/13 23:04:55 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@
 **	Elle devra renvoyer 1 si str est une chaîne vide
 */
 
+#include <string.h>
+#include <stdio.h>
+
 int		ft_str_is_alpha(char *str)
 {
 	int i;
@@ -27,4 +30,15 @@ int		ft_str_is_alpha(char *str)
 		if (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')))
 			return (0);
 	return (1);
+}
+
+int		main(void)
+{
+	printf("%d\n", ft_str_is_alpha("dalGskm"));
+	printf("%d\n", ft_str_is_alpha("dalGa931-skm"));
+	printf("%d\n", ft_str_is_alpha("AalGskmz"));
+	printf("%d\n", ft_str_is_alpha("dalG@#4skm"));
+	printf("%d\n", ft_str_is_alpha("zalGsk@mA"));
+	printf("%d\n", ft_str_is_alpha(""));
+	return 0;
 }
