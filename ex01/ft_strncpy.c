cdd1cf7e98bb33740d50de8f8c06975cb285d28d
Teddy Blanco <tblanco@c2r2p12.42quebec.com>
2021-02-15 16:48:56 -0500

    ex02 int to unsigned int for i

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: tblanco <tblanco@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 20:56:56 by tonted            #+#    #+#             */
-/*   Updated: 2021/02/14 14:35:08 by tblanco          ###   ########.fr       */
+/*   Updated: 2021/02/15 16:48:14 by tblanco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int i;
+	unsigned int i;
 
 	i = -1;
 	while (++i < n && src[i] != '\0')
