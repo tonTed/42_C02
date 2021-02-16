14c05ad3e6b847286e2c5d8ff0ff0627714c03a0
tonted <yddetco@gmail.com>
2021-02-15 21:04:51 -0500

    ex10 norminette

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: tonted <tonted@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/13 21:25:53 by tonted            #+#    #+#             */
-/*   Updated: 2021/02/15 21:03:34 by tonted           ###   ########.fr       */
+/*   Updated: 2021/02/15 21:04:32 by tonted           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 ** Reproduire à l’identique le fonctionnement de la fonction strlcpy
 ** (man strlcpy).
 */
-#include <string.h>
-#include <stdio.h>
 
 int				ft_strlen(char *str)
 {
@@ -41,20 +39,3 @@ unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 	dest[--i] = '\0';
 	return (len_src);
 }
-
-int main()
-{
-	char dest[] = "987654321";
-	char src[] = "123456";
-	int return1 = strlcpy(dest, src, 10);
-	printf("%s (%d)\n", dest, return1);
-
-	char dest2[] = "987654321";
-	char src2[] = "123456";
-	int return2 = ft_strlcpy(dest2, src2, 10);
-	printf("%s (%d)\n", dest2, return2);
-
-
-	return 0;
-}
-
